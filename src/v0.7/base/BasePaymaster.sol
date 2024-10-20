7b6f232d693b54e38c2bc252a5aa08d3545e3b00
Shunsuke Watanabe <shunsukesan.tech@gmail.com>
2024-10-20 16:21:42 +0100

    install submodules

--- src/v0.7/base/BasePaymaster.sol
+++
@@ -1,5 +0,0 @@
-import {IPaymaster} from "@account-abstraction-v07/interfaces/IPaymaster.sol";
-
-contract BasePaymaster is IPaymaster, Ownable {
-
-}
---
+++ src/v0_7/base/BasePaymaster.sol
@@ -0,0 +1,5 @@
+// import {IPaymaster} from "@account-abstraction-v07/interfaces/IPaymaster.sol";
+
+// contract BasePaymaster is IPaymaster, Ownable {
+
+// }
