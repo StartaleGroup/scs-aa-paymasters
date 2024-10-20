523ecc6aeb5777641088fdcf3eb5e52e47d8be47
Shunsuke Watanabe <shunsukesan.tech@gmail.com>
2024-10-20 16:29:04 +0100

    fix project structure

--- src/v0_7/base/BasePaymaster.sol
+++ src/v0_7/base/BasePaymaster.sol
@@ -1,5 +1,9 @@
-// import {IPaymaster} from "@account-abstraction-v07/interfaces/IPaymaster.sol";
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
 
-// contract BasePaymaster is IPaymaster, Ownable {
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IPaymaster} from "@account-abstraction/v0_7/contracts/interfaces/IPaymaster.sol";
 
-// }
+contract BasePaymaster is IPaymaster, Ownable {
+
+}
