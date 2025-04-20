2dedd9d56080a1fa563755c5c42fce86be75a771
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2025-04-20 05:14:40 +0400

    feat: postpaid paymaster contract added

---
+++ src/interfaces/IStartaleManagedPaymaster.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {PackedUserOperation} from "@account-abstraction/contracts/core/UserOperationLib.sol";
+import {IStartaleManagedPaymasterEventsAndErrors} from "./IStartaleManagedPaymasterEventsAndErrors.sol";
+
+/**
+ * @title IStartaleManagedPaymaster
+ * @notice Interface for the StartaleManagedPaymaster that enables sponsored transactions
+ * @dev Extends IStartaleManagedPaymasterEventsAndErrors to include main functionality
+ */
+interface IStartaleManagedPaymaster is IStartaleManagedPaymasterEventsAndErrors {
+    /**
+     * @notice Adds a new authorized signer for validating operations
+     * @param _signer The address to add as a signer
+     */
+    function addSigner(address _signer) external payable;
+
+    /**
+     * @notice Removes an authorized signer
+     * @param _signer The signer address to remove
+     */
+    function removeSigner(address _signer) external payable;
+
+    /**
+     * @notice Withdraws ERC20 tokens from the paymaster
+     * @dev Can be used to recover accidentally sent tokens
+     * @param _token The token contract to withdraw from
+     * @param _target The recipient address
+     * @param _amount The amount to withdraw
+     */
+    function withdrawERC20(IERC20 _token, address _target, uint256 _amount) external;
+
+    /**
+     * @notice Withdraws ETH from the paymaster
+     * @param _recipient The recipient address
+     * @param _amount The amount of ETH to withdraw
+     */
+    function withdrawEth(address payable _recipient, uint256 _amount) external payable;
+
+
+    /**
+     * @notice Generates a hash of the given UserOperation to be signed by the paymaster
+     * @param _userOp The UserOperation structure
+     * @param _validUntil The timestamp until which the operation is valid
+     * @param _validAfter The timestamp after which the operation is valid
+     * @return The hashed UserOperation data
+     */
+    function getHash(
+        PackedUserOperation calldata _userOp,
+        uint48 _validUntil,
+        uint48 _validAfter
+    ) external view returns (bytes32);
+
+    /**
+     * @notice Parses the paymaster data to extract relevant information
+     * @param _paymasterAndData The encoded paymaster data
+     * @return validUntil The timestamp until which the operation is valid
+     * @return validAfter The timestamp after which the operation is valid
+     * @return signature The signature validating the operation
+     */
+    function parsePaymasterAndData(bytes calldata _paymasterAndData)
+        external
+        pure
+        returns (
+            uint48 validUntil,
+            uint48 validAfter,
+            bytes calldata signature
+        );
+}
---
+++ src/sponsorship/StartaleManagedPaymaster.sol
@@ -0,0 +1,173 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import {SignatureCheckerLib} from "solady/utils/SignatureCheckerLib.sol";
+import {ECDSA as ECDSA_solady} from "solady/utils/ECDSA.sol";
+import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
+import {BasePaymaster} from "../base/BasePaymaster.sol";
+import {UserOperationLib, PackedUserOperation} from "@account-abstraction/contracts/core/UserOperationLib.sol";
+import {_packValidationData} from "@account-abstraction/contracts/core/Helpers.sol";
+import {IEntryPoint} from "@account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import {ReentrancyGuardTransient} from "@openzeppelin/contracts/utils/ReentrancyGuardTransient.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {MultiSigners} from "../lib/MultiSigners.sol";
+import {IStartaleManagedPaymaster} from "../interfaces/IStartaleManagedPaymaster.sol";
+
+contract StartaleManagedPaymaster is BasePaymaster, MultiSigners, ReentrancyGuardTransient, IStartaleManagedPaymaster {
+    using UserOperationLib for PackedUserOperation;
+    using SignatureCheckerLib for address;
+    using ECDSA_solady for bytes32;
+
+    uint256 private constant VALID_TIMESTAMP_OFFSET = PAYMASTER_DATA_OFFSET;
+
+    uint256 private constant SIGNATURE_OFFSET = VALID_TIMESTAMP_OFFSET + 64;
+
+    event UserOperationSponsored(bytes32 indexed userOpHash,address indexed user);
+
+    /**
+     * @notice Initializes the SponsorshipPaymaster contract
+     * @param _owner The owner of the paymaster
+     * @param _entryPoint The ERC-4337 EntryPoint contract address
+     * @param _signers Array of authorized signers for paymaster validation
+     */
+    constructor(
+        address _owner,
+        address _entryPoint,
+        address[] memory _signers
+    ) BasePaymaster(_owner, IEntryPoint(_entryPoint)) MultiSigners(_signers) {
+    }
+
+    /**
+     * @notice Receives ETH payments
+     * @dev Silent receive function (no events to save gas)
+     */
+    receive() external payable {
+        // do nothing
+        // unnecessary to emit that consume gas
+    }
+
+    /**
+     * @notice Adds a new signer to the list of authorized signers
+     * @param _signer The address of the signer to add
+     */
+    function addSigner(address _signer) external payable onlyOwner {
+        _addSigner(_signer);
+    }
+
+    /**
+     * @notice Removes a signer from the list of authorized signers
+     * @param _signer The address of the signer to remove
+     */
+    function removeSigner(address _signer) external payable onlyOwner {
+        _removeSigner(_signer);
+    }
+
+    /**
+     * @notice Withdraws ETH from the paymaster
+     * @param _recipient The recipient address
+     * @param _amount The amount of ETH to withdraw
+     */
+    function withdrawEth(address payable _recipient, uint256 _amount) external payable onlyOwner nonReentrant {
+        if (_recipient == address(0)) {
+            revert InvalidWithdrawalAddress();
+        }
+        (bool success,) = _recipient.call{value: _amount}("");
+        if (!success) {
+            revert WithdrawalFailed();
+        }
+        emit EthWithdrawn(_recipient, _amount);
+    }
+
+    /**
+     * @notice Withdraws ERC20 tokens from the paymaster
+     * @param _token The token contract to withdraw from
+     * @param _target The recipient address
+     * @param _amount The amount to withdraw
+     */
+    function withdrawERC20(IERC20 _token, address _target, uint256 _amount) external onlyOwner nonReentrant {
+        _withdrawERC20(_token, _target, _amount);
+    }
+
+    /**
+     * return the hash we're going to sign off-chain (and validate on-chain)
+     * this method is called by the off-chain service, to sign the request.
+     * it is called on-chain from the validatePaymasterUserOp, to validate the signature.
+     * note that this signature covers all fields of the UserOperation, except the "paymasterAndData",
+     * which will carry the signature itself.
+     */
+    function getHash(PackedUserOperation calldata userOp, uint48 validUntil, uint48 validAfter)
+    public view returns (bytes32) {
+        //can't use userOp.hash(), since it contains also the paymasterAndData itself.
+        address sender = userOp.getSender();
+        return
+            keccak256(
+            abi.encode(
+                sender,
+                userOp.nonce,
+                keccak256(userOp.initCode),
+                keccak256(userOp.callData),
+                userOp.accountGasLimits,
+                uint256(bytes32(userOp.paymasterAndData[PAYMASTER_VALIDATION_GAS_OFFSET : PAYMASTER_DATA_OFFSET])),
+                userOp.preVerificationGas,
+                userOp.gasFees,
+                block.chainid,
+                address(this),
+                validUntil,
+                validAfter
+            )
+        );
+    }
+
+    /**
+     * @notice Validates the UserOperation and deducts the required gas sponsorship amount
+     * @param _userOp The UserOperation being validated
+     * @return Encoded context for post-operation handling and validationData for EntryPoint
+     */
+    function _validatePaymasterUserOp(
+        PackedUserOperation calldata _userOp,
+        bytes32 userOpHash,
+        uint256 /*_requiredPreFund*/
+    ) internal override returns (bytes memory, uint256) {
+
+        (uint48 validUntil, uint48 validAfter, bytes calldata signature) = parsePaymasterAndData(_userOp.paymasterAndData);
+
+        if (signature.length != 64 && signature.length != 65) {
+            revert PaymasterSignatureLengthInvalid();
+        }
+
+        address recoveredSigner = (
+            (getHash(_userOp, validUntil, validAfter).toEthSignedMessageHash()).tryRecover(
+                signature
+            )
+        );
+
+        if (recoveredSigner == address(0)) {
+            revert PotentiallyMalformedSignature();
+        }
+
+        bool isValidSig = signers[recoveredSigner];
+
+        // Review: If we need to emit additional details
+        // We could potentially get project specific details (signed/unsigned) from the paymasterAndData
+        emit UserOperationSponsored(userOpHash, _userOp.getSender());
+
+        return ("", _packValidationData(!isValidSig, validUntil, validAfter));
+    }
+
+    function parsePaymasterAndData(bytes calldata paymasterAndData) public pure returns (uint48 validUntil, uint48 validAfter, bytes calldata signature) {
+        (validUntil, validAfter) = abi.decode(paymasterAndData[VALID_TIMESTAMP_OFFSET :], (uint48, uint48));
+        signature = paymasterAndData[SIGNATURE_OFFSET :];
+    }
+
+    /**
+     * @notice Internal function to withdraw ERC20 tokens
+     * @param _token The token to withdraw
+     * @param _target The address to send tokens to
+     * @param _amount The amount to withdraw
+     */
+    function _withdrawERC20(IERC20 _token, address _target, uint256 _amount) private {
+        if (_target == address(0)) revert InvalidWithdrawalAddress();
+        SafeTransferLib.safeTransfer(address(_token), _target, _amount);
+        emit TokensWithdrawn(address(_token), _target, msg.sender, _amount);
+    }
+}
