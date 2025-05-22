b1c9ba9f4e2186dbbf8dfed6860f3262852daf0f
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2025-05-22 10:51:34 +0530

    feat: bump solidity version (#41)

--- test/foundry/TestCounter.sol
+++ test/foundry/TestCounter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.29;
+pragma solidity ^0.8.30;
 
 //sample "receiver" contract, for testing "exec" from account.
 contract TestCounter {
--- test/foundry/unit/fuzz/TestFuzz_SponsorshipPaymaster.t.sol
+++ test/foundry/unit/fuzz/TestFuzz_SponsorshipPaymaster.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.29;
+pragma solidity ^0.8.30;
 
 import "../../TestBase.sol";
 import {ISponsorshipPaymaster} from "../../../../src/interfaces/ISponsorshipPaymaster.sol";
